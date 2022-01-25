3fd86e545e0d8e785104b6781a7e6587c48948a5
Yue Cao <yue.cao@my.jcu.edu.au>
2022-01-25 16:58:37 +0800

    Create a new route so that you can enter Celsius values in the URL and see the Fahrenheit values in the Web page

--- temperatures.py
+++ temperatures.py
@@ -1,36 +1,14 @@
-MENU = """C - Convert Celsius to Fahrenheit
-    F - Convert Fahrenheit to Celsius
-    Q - Quit"""
-print(MENU)
+from flask import Flask
+
+app = Flask(__name__)
 
 
-def main():
-    """Temperature converting program"""
-    choice = input(">>> ").upper()
-    while choice != "Q":
-        if choice == "C":
-            celsius = float(input("Celsius: "))
-            fahrenheit = convert_c_to_f(celsius)
-            print("Result: {:.2f} F".format(fahrenheit))
-        elif choice == "F":
-            fahrenheit = float(input("Fahrenheit: "))
-            celsius = convert_f_to_c(fahrenheit)
-            print("Result: {:.2f} C".format(celsius))
-        else:
-            print("Invalid option")
-        print(MENU)
-        choice = input(">>> ").upper()
-    print("Thank you.")
-
-
-def convert_c_to_f(celsius):
+@app.route('/f/<celsius>')
+def convert_c_to_f(celsius=100):
     """Convert celsius to fahrenheit"""
-    return celsius * 9.0 / 5 + 32
+    fahrenheit = float(celsius) * 9 / 5 + 32
+    return str(fahrenheit)
 
 
-def convert_f_to_c(fahrenheit):
-    """Convert fahrenheit to celsius"""
-    return 5 / 9 * (fahrenheit - 32)
-
-
-main()
+if __name__ == '__main__':
+    app.run()
